b6195c9cf329bdce6acd3c80831b0d9eaea7c599
Juan Hernandez <juanher0825@gmail.com>
2022-11-15 08:39:14 -0500

    The register view is implemented

---
+++ app/src/main/java/com/example/cocopark/RegisterActivity.kt
@@ -0,0 +1,16 @@
+package com.example.cocopark
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+import com.example.cocopark.databinding.ActivityRegisterBinding
+
+class RegisterActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRegisterBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
